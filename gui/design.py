aa8b3f1a41f0f2c55d6acf0065a254094c460d9f
HappyProLoToS <56304317+HappyProLoToS@users.noreply.github.com>
2019-11-05 22:00:19 +0300

    Update design.py

--- gui/design.py
+++ gui/design.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'gui/mainwindow.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.2
-#
-# WARNING! All changes made in this file will be lost!
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
 
